6a7f9238fa9bffbfa41fe44c1e15209698d52444
Prakhar Kapoor <kapoorprakhar99@gmail.com>
2022-03-22 15:03:28 +0530

    my first repo

---
+++ Converter.java
@@ -0,0 +1,13 @@
+class Fibonacci
+{  
+public static void main(String args[])  
+{    
+ double r,d;
+ System.out.print("Enter dollar:");
+ Scanner sc = new Scanner(System.in);
+ d = sc.nextInt();
+ System.out.print("In rupees:");
+ r = d* 76.40;
+ System.out.print(r);
+}
+}
---
+++ Factorial.java
@@ -0,0 +1,12 @@
+class Factorial
+{  
+public static void main(String args[])  
+{    
+ int p=1,i,count=5;       
+ for(i=1;i<=count;i++)    
+ {    
+  p*=i;    
+ }
+ System.out.print("Factorial of 5 is "+p);
+}
+}
